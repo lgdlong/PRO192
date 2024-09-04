1109cec9ea22c868b577531f4027163eb72f34ed
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2024-09-04 14:37:45 +0700

    test

--- test/Main.java
+++ test/Main.java
@@ -1,5 +1,20 @@
+class Human {
+    int id;
+    String name;
+
+    public void display() {
+        System.out.print(id + " " + name);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Human man = new Human();
+
+        man.id = 10;
+        man.name = "Long";
+
+        man.display();
     }
+    
 }
